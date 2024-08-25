7ba0ea13e8b30d6acc1fe3e6c3cfee60c96d1f24
jena-shreyas <jenashreyas@gmail.com>
2024-08-25 18:01:32 +0000

    fix inf script

--- scripts/inference/PerceptionTest/perceptiontest_llavanextvideo.sh
+++ scripts/inference/PerceptionTest/perceptiontest_llavanextvideo.sh
@@ -3,7 +3,7 @@ PARTS=4
 
 # python data_prep/split_data_files.py $PARTS
 
-for n in 1; do
+for n in 2 3 4; do
     python experiments/llavanextvideo_perceptiontest_mcqa.py $n > \
         logs/inference/inference_perceptiontest_mcqa_llavanextvideo_p$n.log 2>&1 &
 done
