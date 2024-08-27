3612fe0dcd7b2a50277eb0ef5bd919a9dfb27010
jena-shreyas <jenashreyas@gmail.com>
2024-08-27 11:33:04 +0000

    fix download script

--- data_prep/CinePile/download_dataset.py
+++ data_prep/CinePile/download_dataset.py
@@ -4,14 +4,86 @@ import random
 import subprocess
 random.seed(42)
 
-import pandas as pd
-import numpy as np
+from tqdm import tqdm
 
 from datasets import load_dataset
 
+def download_video(video_url, filename, root):
+    """
+    Download and convert a video from a URL and save it to a specified directory.
+
+    Parameters:
+    - video_url (str): The URL of the video to be downloaded.
+    - filename (str): The base name for the output file, without file extension.
+    - root (str): The root directory where the 'yt_videos' folder will be created.
+
+    Returns:
+    - tuple: A tuple containing the video URL and a boolean. The boolean is True if the
+      download and conversion was successful, and False otherwise.
+    """
+
+    dir_path=root
+
+    try:
+        vid_prefix = os.path.join(dir_path, filename)
+        full_command = [
+            "yt-dlp",
+            "-S",
+            "height:224,ext:mp4:m4a",
+            "--recode",
+            "mp4",
+            "-o",
+            f"{vid_prefix}.mp4",
+            video_url
+        ]
+
+        print(f'saving path: {vid_prefix}.mp4')
+
+        result = subprocess.run(full_command, capture_output=True, text=True)
+
+        if result.returncode == 0:
+            print(f"Downloaded: {vid_prefix}; {video_url}")
+            return video_url, True
+        else:
+            print(f"Failed to download or convert {video_url}. Error: {result.stderr}")
+            return video_url, False
+
+    except Exception as e:
+        print(f"Exception during download or conversion of {video_url}: {e}")
+        return video_url, False
+    
 ## load the dataset
 DATA_ROOT=osj(osd(osd(os.getcwd())),"datasets/CinePile")
-print(DATA_ROOT)
+SPLIT="test"
 
-os.makedirs(DATA_ROOT, exist_ok=True)
 cinepile = load_dataset("tomg-group-umd/cinepile", cache_dir=DATA_ROOT)
+cinepile_data = cinepile[SPLIT]
+vid2freq = {}
+maxfreq = 0
+
+# DATASET SPLIT VIDEO STATISTICS
+for split in ['train', 'test']:
+    cinepile_data = cinepile[split]
+    for i in tqdm(range(len(cinepile_data))):
+        data = cinepile_data[i]
+        clip_title, yt_link = data['yt_clip_title'], data['yt_clip_link']
+        vid = yt_link.split('=')[-1]
+
+        if vid not in vid2freq:
+            vid2freq[vid]=0
+        vid2freq[vid]+=1
+
+        maxfreq = max(maxfreq, vid2freq[vid])
+
+    print(f"Split: {split}")
+    print("Total unique videos : ", len(vid2freq))
+    print("Max frequency : ", maxfreq)
+
+root_dir = osj(DATA_ROOT, f"yt_videos/{SPLIT}")
+os.makedirs(root_dir, exist_ok=True)
+
+for vid in vid2freq:
+    yt_link = f"https://www.youtube.com/watch?v={vid}"
+    vid_path = f"{data['movie_name']}_{yt_link.split('/')[-1]}"
+    if not os.path.exists(os.path.join(root_dir, vid_path + '.mp4')):
+        download_video(yt_link, vid_path, root=root_dir)
